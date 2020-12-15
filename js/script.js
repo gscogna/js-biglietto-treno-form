2b7392fddd1670dae6b10ef2f855c3e2da0da74d
Gennaro <scognamiglioge@hotmail.it>
2020-12-15 14:53:31 +0100

    Start iniziale

---
+++ js/script.js
@@ -0,0 +1,7 @@
+// Creiamo un finto biglietto del treno con:
+// Nome passeggero
+// Codice treno (numero casuale tra 90000 e 100000 escluso)
+// Numero carrozza
+// Prezzo calcolato
+// Categoria selezionata dall'utente
+// Aggiungiamo una piccola animazione al click su "Crea" e "Annulla", se clicchiamo su annulla dobbiamo ripulire il form.
