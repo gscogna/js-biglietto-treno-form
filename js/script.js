cec9c5fa2505f641f54c304ebeeb7233bb1b4e5a
Gennaro <scognamiglioge@hotmail.it>
2020-12-15 18:20:22 +0100

    Bloccato.

--- js/script.js
+++ js/script.js
@@ -5,43 +5,58 @@
 // Prezzo calcolato
 // Categoria selezionata dall'utente
 // Aggiungiamo una piccola animazione al click su "Crea" e "Annulla", se clicchiamo su annulla dobbiamo ripulire il form.
+var element = document.getElementById("genera");
+
+element.addEventListener('click',
+function() {
 
 
+    var nome = document.getElementById('nome').value;
+    var km = document.getElementById('kilometri').value;
+    var eta = document.getElementById('eta').value;
 
-var nome = document.getElementById('nome').value;
-var km = document.getElementById('kilometri').value;
-var eta = document.getElementById('eta').value;
-
-
-
-var costoBiglietto = km * 0.21;
-var tariffa = 'tariffa standard';
+    var costoBiglietto = km * 0.21;
+    var tariffa = 'tariffa standard';
 
 
-if (eta == 'minorenne'){
+    if (eta == 'minorenne'){
 
-    costoBiglietto =  costoBiglietto - km * 20/100;
-    tariffa = 'sconto minorenne';
+        costoBiglietto =  costoBiglietto - km * 20/100;
+        tariffa = 'sconto minorenne';
 
-}else if{(eta == 'over' )
+    }else if (eta == 'over' ){
 
-    costoBiglietto =  costoBiglietto - km * 40/100;
-    tariffa = 'sconto silver';
+        costoBiglietto =  costoBiglietto - km * 40/100;
+        tariffa = 'sconto silver';
+
+    }
+
+    // inserire dati input
+    document.getElementById('passeggero-name').innerHTML = nome;
+
+    document.getElementById('tipo-offerta').innerHTML = tariffa;
+
+    document.getElementById('carrozza').innerHTML =
+    Math.floor(Math.random( ) * 9) + 1;
+
+    document.getElementById('codice-cp').innerHTML =
+    function getRandomInt(min, max) {
+      min = Math.ceil(9000);
+      max = Math.floor(9900);
+      return Math.floor(Math.random( ) * (9900 - 9000)) + 9001;
+    }
+
+    document.getElementById('costo').innerHTML =
+    costoBiglietto.tofixed(2) + ' €';
 
 }
+);
 
 
-// inserire dati input
-document.getElementById('passeggero-name').innerHTML = nome;
 
-document.getElementById('tipo-offerta').innerHTML = tariffa;
+var element = document.getElementById("annulla");
+element.addEventListener('click',
+function() {
 
-document.getElementById('carrozza').innerHTML =
-Math.floor(Math.random( ) * 9) + 1;
-
-document.getElementById('codice-cp').innerHTML =
-Math.floor(Math.random( ) * 6000) + 1;
 }
-
-document.getElementById('costo').innerHTML =
-costoBiglietto.tofixed(2) + ' €';
+);
